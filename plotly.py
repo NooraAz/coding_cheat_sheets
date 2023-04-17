8af9565555d92a23332dd2f826ad8d661542418e
Noora Azadvari <e.azadvari98@gmail.com>
2023-04-17 16:08:27 -0700

    Update plotly.py

--- plotly.py
+++ plotly.py
@@ -3,3 +3,9 @@ import plotly.express as px
 # histogram
 fig = px.histogram(df, x='col1')
 fig.show()
+
+# treating x as catogorical data
+fig.update_layout(xaxis_type='category')
+
+# background color
+fig.update_layout(plot_bgcolor='rgba(0, 0, 0, 0)')
