bf2cdfbd28b9d3b12c4355b065b7644bd776f2e6
Noora Azadvari <e.azadvari98@gmail.com>
2023-04-21 10:00:59 -0700

    Update numpy.py

--- numpy.py
+++ numpy.py
@@ -6,3 +6,5 @@ arr = np.empty((2,2),bool)
 # create an all-False array
 arr = np.full((2,2),False)
 
+# gettig unique values and their count from an a list
+values, counts = np.unique(arr, return_counts= True)
