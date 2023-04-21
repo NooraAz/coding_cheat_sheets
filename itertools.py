88ff4f1bf3d5d815ff00ca652ee33f3ae5a084c6
Noora Azadvari <e.azadvari98@gmail.com>
2023-04-21 09:59:42 -0700

    Update itertools.py

--- itertools.py
+++ itertools.py
@@ -1,1 +1,15 @@
+import itertools as it
 
+# different iterations
+
+# product: AA,AB,AC,BA,BB,BC,CA,CB,CC
+it.product('ABC',repeat=2) 
+           
+# permutations: AB,AC,BA,BC,CA,CB
+it.permutations('ABC',2)
+
+# combinations: AB,AC,BC
+it.combinations('ABC',2)
+
+# combinations_with_replacement: AA,AB,AC,BB,BC,CC
+it.combinations_with_replacement('ABC',2)
