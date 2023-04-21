412e7f33f0be81139627dd5b637439f82c5247df
Noora Azadvari <e.azadvari98@gmail.com>
2023-04-21 10:04:24 -0700

    Update pyrosetta.py

--- pyrosetta.py
+++ pyrosetta.py
@@ -1,12 +1,14 @@
 # getting coordinates of an atom in pose
-
 mypose.residue(i).xyz(mypose.residue(i).atom_index('O'))
 
 ## getting torsion angle between 4 atoms
-
 # method 1 - error: can't find degree of freedom
 atom1 = #atomID
 mypose.conformation().torsion_angle(atom1,atom2,atom3,atom4)
-
 # method 2
 numeric.dihedral_degrees(xyz1,xyz2,xyz3,xyz4)
+
+# getting bond length between two atoms
+# a and b are atomIDs
+# atomID(i,j): i is atom index and j is resuidue index
+mypose.conformation().bond_length(a,b)
