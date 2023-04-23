5cf8642f5c79934aa0aae6c4082df1efd5b17d82
Noora Azadvari <e.azadvari98@gmail.com>
2023-04-23 07:50:54 -0700

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -5,6 +5,16 @@ df = pd.DataFrame()
 # getting rows by their index
 df.take([0,1,2,3])
 
+# getting rows by their column value
+## if column value is in this list
+df[df['col1'].isin(mylist)]
+## if column value -is not- in this list
+df[~df['col1'].isin(mylist)]
+## ranges (the point is to use & | operators instead of "and" , "or")
+df[(df['col'] < 10) & (df['col'] > 1)]
+df[(df['col'] > 10) | (df['col'] < 1)]
+
+
 # sort values by column
 df.sort_values(by=['col1'])
 
