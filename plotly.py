389c1246e93e4d05c689c7f876139c6a90b025cb
Noora Azadvari <e.azadvari98@gmail.com>
2023-04-23 07:53:06 -0700

    Update plotly.py

--- plotly.py
+++ plotly.py
@@ -4,8 +4,17 @@ import plotly.express as px
 fig = px.histogram(df, x='col1')
 fig.show()
 
+# adding horizontal and vertical lines
+fig.add_hline(y=1)
+fig.add_vline(x=1)
+
+# setting range of axes
+fig.update_xaxes(range=[0,1])
+fig.update_yaxes(range=[0,1])
+
 # treating x as catogorical data
 fig.update_layout(xaxis_type='category')
 
 # background color
 fig.update_layout(plot_bgcolor='rgba(0, 0, 0, 0)')
+
