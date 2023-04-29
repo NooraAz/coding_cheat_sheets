b95ee290543e2fdd4dc5038ae9d909f9b41d85cb
Noora Azadvari <e.azadvari98@gmail.com>
2023-04-28 18:13:42 -0700

    Update plotly.py

--- plotly.py
+++ plotly.py
@@ -2,7 +2,13 @@ import plotly.express as px
 
 # histogram
 fig = px.histogram(df, x='col1')
-fig.show()
+# histogram with different colored data
+fig = px.histogram(df, x="total_bill", color="sex")
+
+# violin plot
+fig = go.Figure()
+for n,l in zip(names,lists):
+    fig.add_trace(go.Violin(y=df['total_bill'][df['day'] == day],name=n,box_visible=True,meanline_visible=True))
 
 # adding horizontal and vertical lines
 fig.add_hline(y=1)
@@ -18,3 +24,5 @@ fig.update_layout(xaxis_type='category')
 # background color
 fig.update_layout(plot_bgcolor='rgba(0, 0, 0, 0)')
 
+# using themes
+fig.update_layout(template="simple_white") 
