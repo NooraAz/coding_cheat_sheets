5159813590214ab835edc77df341ad21758df081
Noora Azadvari <e.azadvari98@gmail.com>
2023-05-05 14:42:52 -0700

    Update plotly.py

--- plotly.py
+++ plotly.py
@@ -10,6 +10,12 @@ fig = go.Figure()
 for n,l in zip(names,lists):
     fig.add_trace(go.Violin(y=df['total_bill'][df['day'] == day],name=n,box_visible=True,meanline_visible=True))
 
+# heatmap (matrix)
+fig = px.imshow(arr)
+
+# line plot
+fig = px.line(df, x="year", y="lifeExp", title='Life expectancy in Canada')
+
 # adding horizontal and vertical lines
 fig.add_hline(y=1)
 fig.add_vline(x=1)
@@ -31,5 +37,4 @@ fig.update_layout(plot_bgcolor='rgba(0, 0, 0, 0)')
 # using themes
 fig.update_layout(template="simple_white") 
 
-# line plot
-fig = px.line(df, x="year", y="lifeExp", title='Life expectancy in Canada')
+
