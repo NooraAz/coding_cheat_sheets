1df0ef46bae19a00ffdfee0beaddc67fe070c47c
Noora Azadvari <e.azadvari98@gmail.com>
2023-07-01 14:01:25 -0700

    Update pyrosetta.py

--- pyrosetta.py
+++ pyrosetta.py
@@ -23,3 +23,15 @@ numeric.dihedral_degrees(xyz1,xyz2,xyz3,xyz4)
 # a and b are atomIDs
 # atomID(i,j): i is atom index and j is resuidue index
 mypose.conformation().bond_length(a,b)
+
+
+# saving poses into an array, reading from silent file
+import subprocess as sp
+sf = rosetta.core.import_pose.pose_stream.SilentFilePoseInputStream(silent_path)
+silent_size = int(sp.getoutput(f"grep '^SCORE:' {silent_path} | wc -l"))
+poses = np.empty((1,silent_size-1),dtype=object)
+counter = 0
+while sf.has_another_pose():
+    poses[0][counter] = Pose()
+    sf.fill_pose(poses[0][counter])
+    counter +=1
