5d658e5e3aac1b5bc805e8e1ea4a8103053e4580
Noora Azadvari <e.azadvari98@gmail.com>
2023-07-03 11:15:40 -0700

    Update math.py

--- math.py
+++ math.py
@@ -4,5 +4,10 @@ import math
 math.pi
 
 # flooring 
-a = math.floor(3.89)
-# returns 3
+a = math.floor(3.89) # returns 3
+
+# convert radians to degrees
+math.degrees(angle)
+# convert degrees to radians
+math.radians(angle)
+
