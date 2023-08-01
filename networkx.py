16748bf219e6c4e5b38b428f03fa957250412c5f
Noora Azadvari <e.azadvari98@gmail.com>
2023-08-01 11:21:32 -0700

    Update networkx.py

--- networkx.py
+++ networkx.py
@@ -1,28 +1,30 @@
 import networkx as nx
 
-# convert adjacency matrix to graph
+# Convert adjacency matrix to graph
 nx.convert_matrix.from_numpy_matrix(arr)
 
 # drawing with positions 
 nx.draw(G, pos=nx.kamada_kawai_layout(G))
 
-# remove a node
+# remove node(s)
 G.remove_node(n)
+G.remove_nodes_from(nodes_list)
 
-# remove edges in an edge list
+# remove edge(s)
+G.remove_edge(u,v)
 edge_list = [(u1,v1),(u2,v2)]
 G.remove_edges_from(edge_list)
 
-# getting neighbors of a node
+# Getting neighbors of a node
 G.neighbors(n) 
-# it returns an iterable object, so you can easily convert it to list 
+# It returns an iterable object, so you can easily convert it to list 
 list(G.neighbors(n))
 
-# changing attribute of a single node
-# n is node label
+# Changing attribute of a single node
+# n is the node label
 nx.set_node_attributes(G, {n:65}, 'weight')
 
-# getting connected components
+# Getting connected components
 S = [G.subgraph(c).copy() for c in nx.connected_components(G)]
 
 # checking if there is a path between two nodes
