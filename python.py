61a2accfd555325bf9e4ca95ae98ffc012dfa9b0
Noora Azadvari <e.azadvari98@gmail.com>
2023-09-06 15:18:24 -0700

    Update python.py

--- python.py
+++ python.py
@@ -1,4 +1,4 @@
-# rounding a number to nearest integar
+# rounding a number to the nearest integer
 double_number = 3.46789269
 round(double_number)
 
@@ -8,19 +8,19 @@ round(1.2345676,2) # --> 1.2
 # rounding with keeping ending zeros
 "%.3f" % 1.2300 # --> 1.230
 
-# removing spaces from beginning and end of string
+# removing spaces from the beginning and end of string
 mystring.strip()
 
-# making a list from a string (by comma for example)
+# Making a list from a string (by a comma for example)
 mystring.split(',')
 
-# sort a list based on second value of sublist
+# Sort a list based on the second value of sublist
 mylist.sort(key= lambda x:x[1])
 
-# sort a list based on size of sublists (large to small)
+# Sort a list based on the size of sublists (large to small)
 my_list.sort(key=len,reverse=True)
 
-# sorting sublists of a list
+# Sorting sublists of a list
 sorted_lst = [sorted(sublist) for sublist in lst]
 
 # text justification
@@ -28,5 +28,12 @@ text = 'Have a good day!"
 text.ljust(5)
 text.rjust(5)
 
-# path to a module
+# Path to a module
 print(my_module.__file__)
+
+# handling errors
+try: 
+  # code
+except Exception as e:
+  print(e) #print error
+  pass # ignore error
