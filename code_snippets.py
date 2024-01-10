c15e675fc3c389a9d73ffaea1f0cd25b310ab3f3
Noora Azadvari <e.azadvari98@gmail.com>
2024-01-09 18:23:02 -0800

    Create code_snippets.py

---
+++ code_snippets.py
@@ -0,0 +1,36 @@
+# getting dihedral angles
+
+def get_phi(pose,resi,resj):
+    C = pose.residue(resi).atom("C").xyz()
+    N = pose.residue(resj).atom("N").xyz()
+    CA = pose.residue(resj).atom("CA").xyz()
+    C2 = pose.residue(resj).atom("C").xyz()
+    phi = rosetta.numeric.dihedral_degrees(C,N,CA,C2)
+    return phi
+
+def get_psi(pose,resi,resj):
+    N = pose.residue(resi).atom("N").xyz()
+    CA = pose.residue(resi).atom("CA").xyz()
+    C = pose.residue(resi).atom("C").xyz()
+    N2 = pose.residue(resj).atom("N").xyz()
+    psi = rosetta.numeric.dihedral_degrees(N,CA,C,N2)
+    return psi
+
+def get_phi_psi(pose: Pose) -> list:
+    torsion_list = []
+    size = pose.size()
+    for i in range(1, size+1):
+        if i == 1:
+            phi = get_phi(pose,size,1)
+            psi = get_psi(pose,i,i+1)
+        elif i == size:
+            phi = get_phi(pose,i-1,i)
+            psi = get_psi(pose,size,1)
+        else:
+            phi = get_phi(pose,i-1,i)
+            psi = get_psi(pose,i,i+1)
+            
+        torsion_list.append(phi)
+        torsion_list.append(psi)
+        
+    return torsion_list
