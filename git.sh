6576b1ad88c390a48723e4f6a35c3905ffdd6bcd
Noora Azadvari <e.azadvari98@gmail.com>
2024-03-07 13:52:58 -0500

    git essential training info

--- git.sh
+++ git.sh
@@ -1,3 +1,56 @@
+# initialize an empty git repository
+git init
+git remote add origin $url
+git push --remote-? origin main
+
+# copying a repo on your local system
+# 1. click on code, copy the url
+# 2. on your local machine:
+git clone $url
+
+# local -> global: stage, commit, push
+## staging
+git add $filename 
+git add . #all changed files
+## check status
+git status
+## commiting
+git commit -m "$message"
+git commit # for multi line
+## pushing
+git push 
+
 # show the current branch
 git rev-parse --abbrev-ref HEAD
+git branch
 
+# the history is saved in .git folder. 
+# don't change anything other than config file. 
+
+
+# getting histrory of changes
+git log
+git log -p #all previous commits
+git log --graph
+git log --oneline
+git log --grep='example'
+
+git checkout $branchname
+
+git revert $commitID
+
+# remove all the changes
+git restore 
+git restore --staged example.txt # getting back from stage area to untracked
+
+#renaming files
+git mv
+
+# viewing the changes in staging area
+git diff --cached
+
+# making git ignore files
+# create a file called .gitignore in root folder
+# add pathes that need to be ignored there.
+
+# merge conflict issues (?)
