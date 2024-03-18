324f6da8cc1cb087badeca2b3b57508500f4435f
Noora Azadvari <e.azadvari98@gmail.com>
2024-03-18 12:01:10 -0400

    std::pair

--- std.cpp
+++ std.cpp
@@ -1,2 +1,7 @@
 using namespace std;
 
+#include <utility>
+std::pair<int,std::string> stundent(26,'Noora');
+std::cout << student.first  << std::endl; // prints 26
+std::cout << student.second << std::endl; // print Noora
+
