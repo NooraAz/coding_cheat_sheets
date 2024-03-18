aea71f43f19cca2f480a5d0e0cc90f8e96362f88
Noora Azadvari <e.azadvari98@gmail.com>
2024-03-18 17:42:44 -0400

    // changing data type

--- std.cpp
+++ std.cpp
@@ -5,3 +5,7 @@ std::pair<int,std::string> stundent(26,'Noora');
 std::cout << student.first  << std::endl; // prints 26
 std::cout << student.second << std::endl; // print Noora
 
+
+// changing data type
+std::static_cast<int>(9.23) // converts the data type of value in () to the type in <>
+
