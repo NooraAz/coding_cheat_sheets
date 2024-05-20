91b7c725250b022622a9e1d5f0f3ad20ce6743e0
Noora Azadvari <e.azadvari98@gmail.com>
2024-05-20 14:24:05 -0400

    showing networkx graph with plotly

--- python-general/networkx.py
+++ python-general/networkx.py
@@ -38,3 +38,27 @@ S = [G.subgraph(c).copy() for c in nx.connected_components(G)]
 nx.has_path(G,n1,n2)
 
 # layouts
+
+# showing networkx graph with plotly
+pos=nx.spring_layout(G,seed=1)
+
+edge_x = []
+edge_y = []
+for e1,e2 in G.edges():
+    x0, y0 = pos[e1]
+    x1, y1 = pos[e2]
+    edge_x.append(x0)
+    edge_x.append(x1)
+    edge_x.append(None)
+    edge_y.append(y0)
+    edge_y.append(y1)
+    edge_y.append(None)
+
+node_x = [pos[node][0] for node in G.nodes()]
+node_y = [pos[node][1] for node in G.nodes()]
+node_text = list(G.nodes())
+
+fig = px.scatter(x=node_x, y=node_y,hover_data=[node_text])
+fig.add_trace(go.Scatter(x=edge_x, y=edge_y, mode='lines',line=dict(width=0.5)))
+fig.update_layout(template='simple_white')
+fig.show()
