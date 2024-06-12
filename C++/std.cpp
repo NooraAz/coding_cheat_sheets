f527a3b8a876819df08e201fb646e848dd376cd5
Noora Azadvari <e.azadvari98@gmail.com>
2024-06-12 19:37:56 -0400

    vector

--- C++/std.cpp
+++ C++/std.cpp
@@ -9,3 +9,10 @@ std::cout << student.second << std::endl; // print Noora
 // changing data type
 std::static_cast<int>(9.23) // converts the data type of value in () to the type in <>
 
+// vector 
+std::vector<int> myVector;
+int a = myVector[1];
+
+// vector of tuples
+std::vector<std::tuple<int, int, int>> vec;
+vec.emplace_back(0, 1, 2);
