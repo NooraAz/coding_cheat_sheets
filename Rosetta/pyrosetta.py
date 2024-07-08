b12062032a993cab57dbee13b646ac760e31f670
Noora Azadvari <e.azadvari98@gmail.com>
2024-07-08 18:02:13 -0400

    0

--- Rosetta/pyrosetta.py
+++ Rosetta/pyrosetta.py
@@ -31,7 +31,7 @@ mypose.residue(i).atom_type(a).element()
 
 ## getting torsion angle between 4 atoms
 # method 1 - error: can't find degree of freedom
-atom1 = #atomID
+atom1 = 0 #atomID
 mypose.conformation().torsion_angle(atom1,atom2,atom3,atom4)
 # method 2
 numeric.dihedral_degrees(xyz1,xyz2,xyz3,xyz4)
