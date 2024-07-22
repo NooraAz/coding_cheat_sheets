2290c461c2ef555e4dc9bba00a38f2f9282aaf6b
Noora Azadvari <e.azadvari98@gmail.com>
2024-07-22 10:47:45 -0400

    organize

--- Python/pandas.py
+++ Python/pandas.py
@@ -14,6 +14,17 @@ df = pd.DataFrame()
 df = pd.DataFrame.from_dict(data) # each item is a column
 df = pd.DataFrame.from_dict(data, orient='index') # each item is a row
 
+# ---------- Naming ----------
+
+# set a row as column names
+df.columns = df.iloc[1]
+df = df.drop(df.index[1]) #(ensure there are no repetitive indices)
+
+# renaming columns
+df.rename(columns={"old name": "new name", "B": "c"})
+
+# ------ Selection tools -------
+
 # getting rows by their index
 df.take([0,1,2,3])
 df.iloc[[4]]
@@ -27,7 +38,7 @@ df[~df['col1'].isin(mylist)]
 df[(df['col'] < 10) & (df['col'] > 1)]
 df[(df['col'] > 10) | (df['col'] < 1)]
 
-
+# -------- Sorting ----------
 
 # sort values by column
 df.sort_values(by=['col1'])
@@ -36,43 +47,47 @@ df.sort_values(by=['col1'])
 df['Frequency'] = df.groupby('CompanyName')['CompanyName'].transform('count')
 df.sort_values(['Frequency', 'CompanyName'], inplace=True, ascending=[False, True])
 
+# loc based on inclusion of a string
+df.loc[df['A'].str.contains("PEP")]
+
+# -------- Adding ------------
 # adding a row to end of dataframe
 df.loc[len(df)] = [value1,value2,value3]
 
-# correlation between two columns
+# adding multiple dataframes side by side based on a column
+from functools import reduce
+data_frames = [df1, df2, df3]
+df_merged = reduce(lambda  left,right: pd.merge(left,right,on=['DATE'],how='outer'), data_frames)
+
+# aggregating multiple columns together
+df['new column'] = df[list_of_columns].agg(''.join, axis=1)
+
+# -------- Removing ----------
 
 # removing rows
 df = df.drop([0, 1])
+
 # removing columns
 df = df.drop(['col1', 'col2'], axis=1)
+
 # removing rows with Nan value in a specific column
-df = df[df['col1'].notna()]
-# or 
+df = df[df['col1'].notna()] # or : 
 df.dropna(subset=['col1'])
 
 # reset indices after removing rows or changing order
 df.reset_index(inplace=True)
 
+# --------- Showing ------------
+
 # printing all rows neatly 
 with pd.option_context('display.max_rows', None, 'display.max_columns', None,'display.precision', 3):
     print(df)
-
-# exporting dataframe to CSV file
-df.to_csv('path/to/saving', index=False)
-
-# set a row as column names
-df.columns = df.iloc[1]
-df = df.drop(df.index[1]) #(ensure there are no repetitive indices)
-
-# renaming columns
-df.rename(columns={"old name": "new name", "B": "c"})
 
 # heatmap
 df_res.apply(pd.to_numeric).style.background_gradient(cmap='Viridis')
 df.style.background_gradient(cmap ='viridis').set_properties(**{'font-size': '20px'}) 
 
-# aggregating multiple columns together
-df['new column'] = df[list_of_columns].agg(''.join, axis=1)
+# ---------- Group by -----------
 
 # group rows based on column name into average of those rows
 df_mean = df.groupby('File name', as_index=False)['Energy'].mean()
@@ -82,9 +97,12 @@ df_result = df.groupby(['a'], as_index=False).agg({'c':['mean','std'],'b':'first
 df_result.columns = ['a','c-mean','c-std','b','d']
 df_result.reindex(columns=sorted(df_result.columns))
 
-# normalization
+# ------------ normalization ------------
 normalized_df=(df-df.min())/(df.max()-df.min()) #min-max
 normalized_df=(df-df.mean())/df.std() #mean
 
-# loc based on inclusion of a string
-df.loc[df['A'].str.contains("PEP")]
+
+# ---------- Exporting ---------
+
+# exporting dataframe to CSV file
+df.to_csv('path/to/saving', index=False)
